8b287f992fd05ed27dc346ddcd423f87543c61c3
heurion <satishkota@heurion.co.uk>
2011-04-08 10:19:35 +0530

    removed duplicate position migration file

--- db/migrate/20110407133251_add_position_to_user_detail_setting.rb
+++
@@ -1,9 +0,0 @@
-class AddPositionToUserDetailSetting < ActiveRecord::Migration
-  def self.up
-    add_column :user_detail_settings, :position, :integer ,:default => 0
-  end
-
-  def self.down
-    remove_column :user_detail_settings, :position
-  end
-end
