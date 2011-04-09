bf072b6bdef22bda577eef048a056d94c1d31993
heurion <satishkota@heurion.co.uk>
2011-04-09 14:07:29 +0530

    multi thumbnails implementation for assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,7 +4,7 @@ class Asset < ActiveRecord::Base
   has_attached_file :upload,
                     :url => "/system/assets/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/system/assets/:id/:style/:basename.:extension",
-                    :styles => {:thumb => Proc.new{|a|a.sizes}}
+                    :styles =>  Proc.new { |a| a.instance.attachment_sizes }
   #Validations
   validates_attachment_presence :upload           
 
@@ -15,6 +15,18 @@ class Asset < ActiveRecord::Base
   def thumbnailable?
     is_thumb = !(upload.content_type =~ %r{^(image|(x-)?application)/(x-png|pjpeg|jpeg|jpg|png|gif)$})
     return !is_thumb
+  end
+
+  def attachment_sizes
+    thumbs = { :thumb => ["100x100"] } 
+    unless sizes.nil?
+      size_arr = self.sizes.split(',')
+      size_arr.each do |size|
+        size = size.strip
+        thumbs[size]=[size]
+      end
+    end
+    thumbs 
   end
 
    #Store Size in which the thumbnail has to be prepared
