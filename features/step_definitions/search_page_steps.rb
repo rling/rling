f8b6c854d0cec3a88a41a3ecc9d7431ba2ded17b
Amit Mandal <amit@heurion.com>
2011-04-28 11:33:05 +0530

    removed unwanted *_page_steps.rb files in features

--- features/step_definitions/search_page_steps.rb
+++
@@ -1,7 +0,0 @@
-Then /^I should see "([^"]*)" on the search page$/ do |arg1|
-  page.find('p',:text=>arg1)
-end
-
-Then /^I sould see "([^"]*)" link on the search page$/ do |arg1|
-  page.find('a',:text=>arg1)
-end
