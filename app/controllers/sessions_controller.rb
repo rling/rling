d4e0f2bbc81eb2799c0b0b63232599dff7d12d42
srinivasa1234 <srinivasa@heurion.com>
2011-12-02 18:45:35 +0530

    changed session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,51 +15,50 @@ end
 def first_user_create
 #Create the First User
    role=Role.where(:role_type=>"Administrator").first
-   u = User.where(:role_id=>role.id).first
-   if u.nil?
-     @user = User.create_setting(params[:user], role)
-     #@user = User.new(params[:user])
-    # @user.login = @user.email if params[:is_login_type_email]
-     #@user.role = role
-    # @user.is_activated = true
+   user = User.where(:role_id=>role.id).first
+   if user.nil?
+     @user = User.new(params[:user])
+     @user.login = @user.email if params[:is_login_type_email]
+     @user.role = role
+     @user.is_activated = true
      if @user.save
       # Save all
       # Login / Email Authentication Setting
-      # setting = Setting.where(:name=>"is_login_type_email").first
-      # setting.setting_value = (params[:is_login_type_email].nil? ? "false" : "true")
-      # setting.save
+       setting = Setting.where(:name=>"is_login_type_email").first
+       setting.setting_value = (params[:is_login_type_email].nil? ? "false" : "true")
+       setting.save
       # User registration Setting
-      # setting = Setting.where(:name=>"allow_user_register_user").first
-       #setting.setting_value = (params[:allow_user_register_user].nil? ? "false" : "true")
-       #setting.save
+       setting = Setting.where(:name=>"allow_user_register_user").first
+       setting.setting_value = (params[:allow_user_register_user].nil? ? "false" : "true")
+       setting.save
       # Welcome email setting
-      # setting = Setting.where(:name=>"send_welcome_email").first
-       #setting.setting_value = (params[:send_welcome_email].nil? ? "false" : "true")
-       #setting.save
+       setting = Setting.where(:name=>"send_welcome_email").first
+       setting.setting_value = (params[:send_welcome_email].nil? ? "false" : "true")
+       setting.save
       # Administrator registration setting
-       #setting = Setting.where(:name=>"allow_admin_register_user").first
-       #setting.setting_value = (params[:allow_admin_register_user].nil? ? "false" : "true")
-       #setting.save
+       setting = Setting.where(:name=>"allow_admin_register_user").first
+       setting.setting_value = (params[:allow_admin_register_user].nil? ? "false" : "true")
+       setting.save
       # User activation setting
-       #setting = Setting.where(:name=>"user_activation_required_on_user").first
-       #setting.setting_value = (params[:user_activation_required_on_user].nil? ? "false" : "true")
-       #setting.save
+       setting = Setting.where(:name=>"user_activation_required_on_user").first
+       setting.setting_value = (params[:user_activation_required_on_user].nil? ? "false" : "true")
+       setting.save
       # Administrator activation setting
-       #setting = Setting.where(:name=>"user_activation_required_on_admin").first
-       #setting.setting_value = (params[:user_activation_required_on_admin].nil? ? "false" : "true")
-       #setting.save
+       setting = Setting.where(:name=>"user_activation_required_on_admin").first
+       setting.setting_value = (params[:user_activation_required_on_admin].nil? ? "false" : "true")
+       setting.save
       # URL Setting
-       #setting = Setting.where(:name=>"site_url").first
-       #setting.setting_value = params[:site_url]
-       #setting.save
+       setting = Setting.where(:name=>"site_url").first
+       setting.setting_value = params[:site_url]
+       setting.save
       # Smtp Setting
-       #setting = Setting.where(:name=>"smtp_settings").first
-       #setting.setting_value = params[:smtp_settings]
-       #setting.save
+       setting = Setting.where(:name=>"smtp_settings").first
+       setting.setting_value = params[:smtp_settings]
+       setting.save
       #View users account information setting
-       #setting = Setting.where(:name=>"allow_view_user_account").first
-       #setting.setting_value = (params[:allow_view_user_account].nil? ? "false" : "true")
-       #setting.save
+       setting = Setting.where(:name=>"allow_view_user_account").first
+       setting.setting_value = (params[:allow_view_user_account].nil? ? "false" : "true")
+       setting.save
        respond_to do |format|
          flash[:notice] = t(:admin_created)
          format.html { redirect_to(login_path) }
@@ -68,7 +67,7 @@ def first_user_create
      else
        respond_to do |format|
         flash[:notice] = t(:error_creating_admin)
-        format.html { render "first_user" }
+        format.html { render :action=>"first_user" }
         format.xml  { render :xml=>@user.errors,:status=>:unprocessable_entity }
        end
      end
@@ -79,6 +78,7 @@ def first_user_create
          format.xml  { head:ok }
        end
    end
+   
 end
 
 # GET /sessions/new
@@ -97,6 +97,7 @@ def new
    end
   end
 end
+ 
 
 # POST /sessions
 # POST /sessions.xml
@@ -104,32 +105,28 @@ def create
   if current_user.nil?
      @user = User.where(:login=>params[:login]).first
      user =User.authenticate(params[:login], params[:password])
-      u=@user
      respond_to do |format|
-      if u.nil?
+      if @user.nil? 
           flash[:notice]= t(:incorrect_login)
-          format.html {render  :new}
-      elsif !u.is_activated
-        flash[:notice]= t(:user_activation_required)
-          format.html {render  :new}
+          format.html {render :action => :new}
+      elsif !@user.is_activated
+	  flash[:notice]= t(:user_activation_required)
+          format.html {render :action => :new}
       elsif user.nil?
-          u1 = u
-          u1.failed_login_count += 1
-          u1.save
+          @user.failed_login_count += 1
+          @user.save
          flash[:notice]= t(:incorrect_password)
-         format.html{ render  :new }
+         format.html{ render :action => :new }
       else
-        u2=@user
          flash[:notice] = t(:logged_in)
-         self.current_user = u2
+         self.current_user = @user
          if params[:remember_me]=="1"
            cookies[:remember_me_code] = {:value => current_user.salt, :expires => 30.days.from_now }
          end
-         User.save_login(u2)
-        # @user.last_login_ip=@user.current_login_ip
-        # @user.current_login_ip=request.remote_ip
-        # @user.login_count+=1
-        # @user.save
+         @user.last_login_ip=@user.current_login_ip
+         @user.current_login_ip=request.remote_ip
+         @user.login_count+=1 
+	 @user.save
          if current_user.admin?
             format.html{ redirect_to admin_path }
          else
