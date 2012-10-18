cdefe8d291aaeb1f9ea8c9bbd6aaf200c7482149
Shiv Shankar <shiva@heurion.com>
2012-10-18 12:59:09 +0530

    fixes

--- public/assets/eye-ab0d3541c4810c218d1ee841be2dca3d.js
+++
@@ -1,7 +0,0 @@
-/**
- *
- * Zoomimage
- * Author: Stefan Petre www.eyecon.ro
- * 
- */
-(function(a){var b=window.EYE=function(){var b={init:[]};return{init:function(){a.each(b.init,function(a,b){b.call()})},extend:function(a){for(var b in a)a[b]!=undefined&&(this[b]=a[b])},register:function(a,c){b[c]||(b[c]=[]),b[c].push(a)}}}();a(b.init)})(jQuery)
